223200ee805d80f1879c6a0dfde558664fb72a1b
Cori Snedecor <coriannas@yahoo.com>
2014-12-22 15:46:00 -0800

    Added test for unique email when user tries to sign up

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -10,7 +10,7 @@ feature "Create a new album" do
   # * [X] There is a link to 'Sign Up' on the homepage
   # * [X] If I fill in my email, password, and password confirmation correctly,
   #   I'm greeted with a message that my account has been created
-  # * [ ] If the password and password confirmation fields do not match, I'm given
+  # * [X] If the password and password confirmation fields do not match, I'm given
   #   an error message
   # * [ ] If my email already exists in the database, I am given a message that I
   #   have an account already
@@ -38,5 +38,22 @@ feature "Create a new album" do
     click_on "Sign up"
 
     expect(page).to have_content "Password confirmation doesn't match Password"
+  end
+
+  scenario "email is already registered" do
+    existing_user = User.create(
+    email: "test@example.com",
+    password: "password"
+    )
+
+    visit root_path
+    click_on "Sign Up"
+
+    fill_in "Email", with: existing_user.email
+    fill_in "Password", with: existing_user.password
+    fill_in "Password confirmation", with: existing_user.password
+    click_on "Sign up"
+
+    expect(page).to have_content("Email has already been taken")
   end
 end
