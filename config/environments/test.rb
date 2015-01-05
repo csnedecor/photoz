19be87043f5242d615cae4b96e7f58f89df7a31e
Cori Snedecor <coriannas@yahoo.com>
2015-01-04 22:00:18 -0500

    Fixes hound issues and merges in edit-albums changes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,11 +38,11 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_TEST_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_TEST_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_TEST_SECRET_ACCESS_KEY']
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['S3_TEST_BUCKET_NAME'],
+      access_key_id: ENV['AWS_TEST_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_TEST_SECRET_ACCESS_KEY']
     }
   }
 end
