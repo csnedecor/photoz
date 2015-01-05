9d92850fcdcdd372dad4756ce89d6a30ab2e9c2b
Cori Snedecor <coriannas@yahoo.com>
2015-01-04 22:04:33 -0500

    Fixes more hound syntax

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,9 +40,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV['S3_TEST_BUCKET_NAME'],
-      access_key_id: ENV['AWS_TEST_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_TEST_SECRET_ACCESS_KEY']
+      bucket: ENV["S3_TEST_BUCKET_NAME"],
+      access_key_id: ENV["AWS_TEST_ACCESS_KEY_ID"],
+      secret_access_key: ENV["AWS_TEST_SECRET_ACCESS_KEY"]
     }
   }
 end
